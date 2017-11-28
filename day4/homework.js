67021f62c69266416d618ae79f3ea543909f9cab
Mike Skelton <skeltonmr43@gmail.com>
2017-11-28 10:34:26 -0500

    javascript functions

--- day4/homework.js
+++ day4/homework.js
@@ -17,3 +17,73 @@
 // }
 //
 // console.log(palindrome2('radar'));
+
+// const sumDigits = (num) => {
+//   const splitNum = (num.split(''));
+//   console.log(+splitNum[0] + +splitNum[1]);
+//
+// }
+//
+// sumDigits('73');
+
+// const pythagTheorem = (side1, side2) => {
+//   const square = Math.pow(side1,2) + Math.pow(side2,2);
+//   console.log(square);
+// }
+//
+// pythagTheorem(3, 4);
+
+
+// const sumArray = (array) => {
+//   const numbers = (array.split(''));
+//   console.log(numbers)
+// }
+//
+// sumArray([1,2,3,4,5]);
+
+
+
+// morning lab
+
+// const printGreeting = (name) => {
+//   const reverseWord = (name.split('').reverse().join('').toLowerCase());
+//   console.log(reverseWord);
+// }
+//
+// printGreeting('greetings from Earth');
+
+// const reverseWords = (sentence) => {
+//   const array = (sentence.split(''));
+//   array.reverse();
+//   sentence = array.join(" ");
+//   console.log(array);
+// }
+//
+// reverseWords('well hello there');
+
+// const reverseWords = (string) => {
+//   const splitString = string.split('');
+//   console.log(splitString);
+//   const reverseSplit = splitString.reverse();
+//   console.log(reverseSplit);
+//   const joinSplit = reverseSplit.join('');
+//   console.log(joinSplit);
+// }
+//
+// reverseWords('america is murica');
+
+const calculate = (num1, num2, string) => {
+  if (string == 'add') {
+    console.log(num1 + num2);
+  } else if (string == 'sub') {
+    console.log(num1 - num2);
+  } else if (string == 'mult') {
+    console.log(num1 * num2);
+  } else if (string == 'div') {
+    console.log(num1 / num2);
+  } else if (string == 'exp') {
+    console.log(Math.pow(num1,num2));
+  }
+}
+
+calculate(2, 3, 'exp');
